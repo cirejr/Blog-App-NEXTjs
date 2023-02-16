307554492979e01a322dd12590d2290c611fcd1c
unknown <juniorcireba@gmail.com>
2023-02-16 16:36:21 +0000

    Setup and Header done!

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,32 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
+import { PostCard, PostWidget, Categories} from '../components'
+
 
 const Home = () => {
+	const posts = [
+		{ title: "React Testing", excerpt:"Learn React"},
+		{ title: "React with Tailwind", excerpt:"Learn React with Tailwind"},
+	]
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>Create Next App</title>
+        <title>Tell Us</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+	  <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+		<div className='lg:col-span-8 col-span-1'>
+			{posts.map((post, index) => <PostCard key={post.title} post={post} />)}
+		</div>
+		<div className='lg:col-span-4 col-span-1'>
+			<div className='lg:sticky relative top-8'>
+				<PostWidget />
+				<Categories />
+			</div>
+		</div>
+	  </div>
     </div>
+
   )
 }
 
