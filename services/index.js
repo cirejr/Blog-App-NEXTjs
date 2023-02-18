ee6a1f21e82a9015aeb7310411485e5ebdcd0bd5
cirejr <juniorcireba@gmail.com>
2023-02-18 23:14:06 +0000

    Front Page Done!

--- services/index.js
+++ services/index.js
@@ -1,4 +1,5 @@
 import { request, gql } from 'graphql-request';
+import { Categories } from '../components';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_PURPLEBLOG_ENDPOINT;
 
@@ -35,4 +36,60 @@ export const getPosts = async () => {
 	const result = await request(graphqlAPI, query);
 
 	return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+	const query = gql`
+		query getPostDetails(){
+			posts(
+				orderBy: createdAt_ASC
+				last: 3
+			) {
+				title
+				featuredImage {
+					url
+				}
+				createdAt
+				slug
+			}
+		}
+	`
+	const result = await request(graphqlAPI, query);
+
+	return result.posts;
+}
+
+export const getSimilarPosts = async () => {
+	const query = gql`
+		query PostDetails($categories: [string], $slug: string){
+			post(
+				where: { slug_not: $slug, AND: { categories_some: { slug_in : $categories }} } 
+				last: 3
+			) {
+				title
+				featuredImage {
+					url
+				}
+				createdAt
+				slug
+			}
+		}
+	`
+	const result = await request(graphqlAPI, query);
+
+	return result.posts;
+}
+
+export const getCategories = async () => {
+	const query = gql`
+		query GetCategories {
+			categories {
+				name
+				slug
+			}
+		}
+	`
+	const result = await request(graphqlAPI, query)
+
+	return result.categories
 }
