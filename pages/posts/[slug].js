9088536aa27612412eef5956bfdf362c20db328f
unknown <juniorcireba@gmail.com>
2023-02-21 10:29:54 +0000

    created the PostDetail components

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { getPosts, getPostDetails } from '../../services';
+import {PostDetail, Categories, PostWidget, CommentsForm, Comments, Author } from '../../components'
+
 const PostDetails = () => {
   return (
-	<div>PostDetails</div>
+	<div className="container mx-auto px-10 mb-8">
+		<div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+			<div className='col-span-1 lg:col-span-8 '>
+				<PostDetail />
+				<Author />
+				<CommentsForm />
+				<Comments />
+			</div>
+			<div className='col-span-1 lg:col-span-4'>
+				<div className='relative lg:sticky top-8'>
+					<PostWidget />
+					<Categories />
+				</div>
+			</div>
+		</div>
+	</div>
   )
 }
 
